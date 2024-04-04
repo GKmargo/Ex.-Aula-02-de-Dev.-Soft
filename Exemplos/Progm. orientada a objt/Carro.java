2ba3ac79a68005c2690c0d2a58738e78463044b3
Auto Logon <autologon@CSED.LAB>
2024-04-04 10:55:12 -0300

    Atualização

--- Exemplos/Progm. orientada a objt/Carro.java
+++ Exemplos/Progm. orientada a objt/Carro.java
@@ -1,6 +1,52 @@
 public class Carro extends Veiculo {
     
-    public String modelo;
-    public double velocidade;
+    //enum é usado para definir algo
+    public static enum Tipo{
+        FERRARI,
+        UNO
+    }
+
+    /** Atributos, Caractéristicas ou propriedades - Sempre privadas **/
+    private Tipo tipo;
+    private String modelo;
+    private double velocidade = 0;
+
+    public double getVelocidade(){
+        return this.velocidade;
+    }
+
+    public String getModelo(){
+        return this.modelo;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public Carro(){
+    }
+
+    public Carro(Tipo tipo){
+        this.tipo = tipo;
+    }
+
+    public Carro(Tipo tipo, String modelo){
+        this.tipo = tipo;
+        this.modelo = modelo;
+    }
+
+    public void acelerar(){
+        if (tipo.equals(Tipo.UNO) && this.velocidade <= 150){
+            this.velocidade++;
+        } else if (tipo.equals(Tipo.FERRARI) && this.velocidade <= 300){
+            this.velocidade ++;
+        }
+    }
+
+    public void freiar(){
+        if (this.velocidade >= 0){
+            this.velocidade--;
+        }
+    }
 
 }
--- Exemplos/Progm. orientada a objt/Programa.java
+++ Exemplos/Progm. orientada a objt/Programa.java
@@ -3,8 +3,8 @@ public class Programa {
        
         /** PROGRAMAÇÃO ESTRUTURADA **/
         
-        String modelo1 = "Mille";
-        String modelo2 = "California";
+        //String modelo1 = "Mille";
+        //String modelo2 = "California";
 
         String[] modelos = {"Mille" , "California"};
         double[] velocidades = {80, 300};
@@ -14,13 +14,47 @@ public class Programa {
 
         /** PROGRAMAÇÃO ORIENTADA A OBJETO **/
 
-        Carro uno = new Carro();
-        uno.modelo = "Mille";
-        uno.velocidade = 80;
+        Carro uno = new Carro(Carro.Tipo.UNO, "Mille");
+        //uno.modelo = "Mille";
+        //uno.velocidade = 80;
+        uno.acelerar();
+        uno.acelerar();
+        uno.acelerar();
+        uno.acelerar();
+        uno.acelerar();
+        uno.acelerar();
+        uno.acelerar();
+        uno.acelerar();
+        System.out.println("Velocidade do UNO: " + uno.getVelocidade());
 
-        Carro ferrari = new Carro();
-        ferrari.modelo = "California";
-        ferrari.velocidade = 300;
+        Carro ferrari = new Carro(Carro.Tipo.FERRARI);
+        //ferrari.modelo = "California";
+        ferrari.setModelo("California");
+        //ferrari.velocidade = 300;
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+        System.out.println("Velocidade da ferrari: " + ferrari.getVelocidade());
+
 
         Pessoa pedro = new Pessoa();
         pedro.nome = "Pedro";
